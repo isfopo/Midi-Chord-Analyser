a470bf7f2203cb2b9ad605d74709881015696d13
Isaac Poole <isaacpoolemusic@gmail.com>
2021-10-28 09:21:00 -0400

    increased the number of notes to output a chord

--- detect.js
+++ detect.js
@@ -5,7 +5,7 @@ const s11 = require("sharp11")
 max.addHandler("detect", (...notes) => {
   const namedNotes = notes.map(tonal.Note.fromMidi);
   max.post(namedNotes);
-  if (namedNotes.length > 1) {
+  if (namedNotes.length > 2) {
     max.outlet("chord", s11.chord.identify(...namedNotes));
   } else {
     max.outlet("chord", "null");
